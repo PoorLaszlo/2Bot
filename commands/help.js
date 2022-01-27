4095b2b67759cbedd5fe837b1eb289d8ec14e62d
PoorLaszlo <79516211+PoorLaszlo@users.noreply.github.com>
2022-01-27 17:43:35 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -39,7 +39,7 @@ module.exports = {
   Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.botconfig.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${client.botconfig.Website}) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | [Dashboard](${client.botconfig.Website}) | By Kókai Tiktok Patrik`);
     if (!args[0]) message.channel.send(Embed);
     else {
       let cmd =
